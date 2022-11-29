7e072d005932e9bb829ee72d3c43c889d48bcbbf
Joao Esperancinha <jofisaes@gmail.com>
2022-11-29 19:19:49 +0100

    It still needs jackson

--- isbn-stacks-rest-kofu-mvc-reactive/src/main/kotlin/org/jesperancinha/spring/ISBNStacksKoFuReactiveLauncher.kt
+++ isbn-stacks-rest-kofu-mvc-reactive/src/main/kotlin/org/jesperancinha/spring/ISBNStacksKoFuReactiveLauncher.kt
@@ -2,15 +2,21 @@ package org.jesperancinha.spring
 
 import org.jesperancinha.spring.IsbnDto.Companion.ISBNS
 import org.jesperancinha.spring.IsbnDto.Companion.SMALL_ISBNS
+import org.springframework.boot.web.reactive.context.ReactiveWebServerApplicationContext
+import org.springframework.context.aot.AotApplicationContextInitializer
 import org.springframework.fu.kofu.reactiveWebApplication
 import org.springframework.fu.kofu.webflux.webFlux
+import org.springframework.http.server.reactive.HttpHandler
+import org.springframework.http.server.reactive.ServerHttpRequest
+import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
 
 val app = reactiveWebApplication {
     beans {
-        bean<ISBNSampleService>()
-        bean<ISBNSampleHandler>()
+        bean<ISBNSampleService>(isPrimary = true)
+        bean<ISBNSampleHandler>(isPrimary = true)
     }
     webFlux {
         port = if (profiles.contains("test")) 8181 else 8080
@@ -35,17 +41,30 @@ class ISBNSampleService {
 }
 
 class ISBNSampleHandler(private val isbnSampleService: ISBNSampleService) {
-    fun hello(request: ServerRequest) = ServerResponse.ok().bodyValue(isbnSampleService.generateMessage())
-    fun json(request: ServerRequest) = ServerResponse.ok().bodyValue(ISBNMessage(isbnSampleService.generateMessage()))
-    fun isbns(request: ServerRequest) = ServerResponse.ok().bodyValue(ISBNS)
-    fun smallIsbns(request: ServerRequest) = ServerResponse.ok().bodyValue(SMALL_ISBNS)
+    fun hello(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok().bodyValue(isbnSampleService.generateMessage())
+
+    fun json(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok().bodyValue(ISBNMessage(isbnSampleService.generateMessage()))
+
+    fun isbns(request: ServerRequest): Mono<ServerResponse> = ServerResponse.ok().bodyValue(ISBNS)
+    fun smallIsbns(request: ServerRequest): Mono<ServerResponse> = ServerResponse.ok().bodyValue(SMALL_ISBNS)
 }
 
-class ISBNStacksKoFuReactiveLauncher{
-    companion object{
+class ISBNStacksKoFuReactiveLauncher {
+    companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             app.run()
+        }
+    }
+
+    class Companion__ApplicationContextInitializer :
+        AotApplicationContextInitializer<ReactiveWebServerApplicationContext> {
+        override fun initialize(applicationContext: ReactiveWebServerApplicationContext) {
+            applicationContext.registerBean(ISBNSampleService::class.java, *arrayOf())
+            applicationContext.registerBean(ISBNSampleHandler::class.java, ISBNSampleService())
+
         }
     }
 }
--- isbn-stacks-rest-kofu-plain/src/main/kotlin/org/jesperancinha/spring/ISBNStacksNothingLauncher.kt
+++ isbn-stacks-rest-kofu-plain/src/main/kotlin/org/jesperancinha/spring/ISBNStacksNothingLauncher.kt
@@ -2,10 +2,13 @@ package org.jesperancinha.spring
 
 import org.jesperancinha.spring.IsbnDto.Companion.ISBNS
 import org.jesperancinha.spring.IsbnDto.Companion.SMALL_ISBNS
+import org.springframework.boot.web.reactive.context.ReactiveWebServerApplicationContext
+import org.springframework.context.aot.AotApplicationContextInitializer
 import org.springframework.fu.kofu.reactiveWebApplication
 import org.springframework.fu.kofu.webflux.webFlux
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
 
 val app = reactiveWebApplication {
     beans {
@@ -25,7 +28,7 @@ val app = reactiveWebApplication {
 }
 
 class SampleHandler {
-    fun hello(request: ServerRequest) = ServerResponse.ok().bodyValue("hello")
+    fun hello(request: ServerRequest): Mono<ServerResponse> = ServerResponse.ok().bodyValue("hello")
 }
 
 
@@ -36,6 +39,10 @@ class ISBNStacksNothingLauncher{
             app.run()
         }
     }
+    class Companion__ApplicationContextInitializer :
+        AotApplicationContextInitializer<ReactiveWebServerApplicationContext> {
+        override fun initialize(applicationContext: ReactiveWebServerApplicationContext) {
+            applicationContext.registerBean(SampleHandler::class.java, *arrayOf())
+        }
+    }
 }
-
-
