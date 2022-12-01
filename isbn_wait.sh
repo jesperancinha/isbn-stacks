33246b7c3cbe13f87e717d2f8399c285b5603b8e
Joao Esperancinha <jofisaes@gmail.com>
2022-12-01 18:55:31 +0100

    ISBN stacks logs

--- isbn_wait.sh
+++ isbn_wait.sh
@@ -4,20 +4,20 @@ function checkServiceByNameAndMessage() {
     name=$1
     message=$2
     docker-compose -p "${GITHUB_RUN_ID}" logs "$name" > "logs"
-    string=$(cat logs)
+    logs=$(cat logs)
     counter=0
     echo "Project $GITHUB_RUN_ID"
     echo -n "Starting service $name "
-    while [[ "$string" != *"$message"* ]]
+    while [[ "$logs" != *"$message"* ]]
     do
       echo -e -n "\e[93m-\e[39m"
       docker-compose -p "${GITHUB_RUN_ID}" logs "$name" > "logs"
-      string=$(cat logs)
+      logs=$(cat logs)
       sleep 1
       counter=$((counter+1))
       if [ $counter -eq 200 ]; then
           echo -e "\e[91mFailed after $counter tries! Cypress tests may fail!!\e[39m"
-          echo "$string"
+          echo "$logs"
           exit 1
       fi
     done
